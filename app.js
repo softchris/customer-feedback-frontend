dc6cf663baea29c20af17d7b8a056e4dd47d3c26
Christoffer Noring <noringc@gmail.com>
2018-04-19 15:34:20 +0100

    adding fonts, updating according to Vipins comments

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
 console.log('app loaded');
 
-const saveElem = document.getElementById('save');
-const contentElem = document.getElementById('content');
-const messageElem = document.getElementById('message');
+let selectedDay;
 
 $(document).ready(() => {
   let pageNo = 1;
   let pages = ['page1','page2','page3','page4'];
   let pageElems = pages.map(p => $(`.${p}`));
+
+  // hide all pages
   pageElems.forEach(p => p.hide());
   $('.page1').show();
+
+  // clear all calendar selections
+  $('.day').removeClass('selected');
+
+  // show navigation previous?
+  $('.navigation').hide();
 
 
   console.log('jquery ready');
 
+  function getAbbr(no) {
+    switch(no) {
+      case '1':
+        return 'st';
+      case '2':
+        return 'nd';
+      case '3':
+        return 'rd';
+      default:
+        return 'th';
+    }
+  }
+
   $('.navigation').bind('click', () => {
     pageElems.forEach(p => p.hide());
     pageNo = pageNo -1 >=1 ? pageNo - 1 : pageNo;
+    if(pageNo > 1) {
+      $('.navigation').show();
+    } else {
+      $('.navigation').hide();
+    }
+
     $(`.page${pageNo}`).show();
   });
 
   $('.action').bind('click', () => {
     pageElems.forEach(p => p.hide());
     pageNo = pageNo +1 <= pages.length ? pageNo + 1 : pageNo;
+
+    if(pageNo > 1) {
+      $('.navigation').show();
+    }
+
     $(`.page${pageNo}`).show();
+
+    if(pageNo === pages.length) {
+      $('.selectedDate').html(`${selectedDay}${getAbbr(selectedDay)} May`);
+    }
 
     console.log('action clicked');
   })
+
+  $('.day').bind('click', (evt) => {
+    const elem = $(evt.target);
+
+    selectedDay = elem.html();
+    $('.day').removeClass('selected');
+    elem.addClass('selected');
+  });
 });
 
 // hookup clicks..
